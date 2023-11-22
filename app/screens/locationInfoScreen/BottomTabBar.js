b4d059883da2a0907424529a8c496bca7d60a697
Dilshan Jayasinghe <dilshan.sj@socio.com.my>
2023-11-22 17:08:43 +0800

    LocationInfoScreen complete (FrontEnd)

--- app/screens/locationInfoScreen/BottomTabBar.js
+++ app/screens/locationInfoScreen/BottomTabBar.js
@@ -5,26 +5,30 @@ function BottomTabBar(){
   return (
     <View style={styles.container}>
       <View style={styles.navBar}>
-        {/* Left Icons */}
-        <View style={styles.iconContainer}>
+        {/* Left side Icons */}
+        <View style={styles.iconContainerHome}>
           <TouchableOpacity onPress={() => console.log('Utama Button Pressed!')}>
             <Image source={require('../../assets/utamaIcon.png')} style={styles.icon} />
           </TouchableOpacity>
+        </View>
+        <View style={styles.iconContainerLocation}>
           <TouchableOpacity onPress={() => console.log('Lokasi Button Pressed!')}>
             <Image source={require('../../assets/lokasiIcon.png')} style={styles.icon} />
           </TouchableOpacity>
         </View>
 
         {/* Center Button */}
-        <TouchableOpacity onPress={() => console.log('Tanya Kasih Button Pressed!')} >
-          <Image source={require('../../assets/tanyaKasihIcon.png')} style={styles.centerIcon} />
+        <TouchableOpacity style={styles.centerButtonContainer} onPress={() => console.log('Tanya Kasih Button Pressed!')}  >
+          <Image source={require('../../assets/tanyaKasihIcon.png')} style={styles.centerButton} />
         </TouchableOpacity>
 
-        {/* Right Icons */}
-        <View style={styles.iconContainer}>
+        {/* Right side Icons */}
+        <View style={styles.iconContainerService}>
           <TouchableOpacity onPress={() => console.log('Perkhidmatan Button Pressed!')}>
-            <Image source={require('../../assets/perkhidmatanIcon.png')} style={styles.largeIcon} />
+            <Image source={require('../../assets/perkhidmatanIcon.png')} style={styles.serviceIcon} />
           </TouchableOpacity>
+        </View>
+        <View style={styles.iconContainerFeedback}>
           <TouchableOpacity onPress={() => console.log('Aduan Button Pressed!')}>
             <Image source={require('../../assets/aduanIcon.png')} style={styles.icon} />
           </TouchableOpacity>
@@ -38,14 +42,15 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: '#FFFFFF',
     position: 'absolute',
+    justifyContent: 'center',
     bottom: 0,
     left: 0,
     right: 0,
-    alignItems: 'center',
+    // alignItems: 'center',
   },
   navBar: {
     flexDirection: 'row',
-    justifyContent: 'space-between',
+    justifyContent: 'space-evenly',
     alignItems: 'center',
     backgroundColor: '#fff',
     // borderRadius: 15,
@@ -59,53 +64,54 @@ const styles = StyleSheet.create({
     height: 60,
     // marginHorizontal: 10,
     marginBottom: 20,
-  },
-  iconContainer: {
-    // padding: 20, 
-    justifyContent: 'center',
-    alignItems: 'center',
-    flexDirection: 'row',
-  },
-  sideIconContainer: {
-    flexDirection: 'row',
-    width: '50%', 
-    justifyContent: 'space-evenly',
+    flex: 1,
   },
   icon: {
-    padding: 20,
+    // padding: 20,
     width: 40,
     height: 40,
     resizeMode: 'contain',
   },
-  largeIcon: {
-    padding: 20,
-    width: 60, 
-    height: 80, 
+  serviceIcon: {
+    // padding: 20,
+    // paddingLeft: 20,
+    width: 75, 
+    height: 75, 
     resizeMode: 'contain', 
   },
-  centerButton: {
-    backgroundColor: '#8A2BE2',
-    borderRadius: 30,
-    padding: 10,
-    marginTop: -30,
+  iconContainerLocation: {
+    paddingRight: 10,
+  },
+  iconContainerHome: {
+    paddingRight: 20,
+  },
+  centerButtonContainer: {
+    width: 60,
+    height: 60,
+    // resizeMode: 'contain',
+    borderRadius: 35, 
+    // backgroundColor: '#8A2BE2',
+    backgroundColor: '#FFF',
     justifyContent: 'center',
     alignItems: 'center',
     shadowColor: '#000',
-    shadowOpacity: 0.1,
+    shadowOpacity: 0.5,
     shadowRadius: 10,
     shadowOffset: { width: 0, height: 0 },
     elevation: 10, // for Android shadow
+    // paddingBottom: 40,
+    marginBottom: 40,
   },
-  centerIcon: {
-    width: 60,
-    height: 60,
+  centerButton: {
+    width: 70,
+    height: 70,
     resizeMode: 'contain',
+    // justifyContent: 'center',
     borderRadius: 35, 
     borderColor: 'white', 
     borderWidth: 3, 
     overflow: 'hidden', 
     backgroundColor: 'white',
-    marginBottom: 30,
   },
 });
 

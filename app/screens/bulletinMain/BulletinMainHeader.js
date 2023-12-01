c4e48cc9630479a99ea2a290f67552ff7c7ebee4
Dilshan Jayasinghe <dilshan.sj@socio.com.my>
2023-12-01 17:01:03 +0800

    Added header component

---
+++ app/screens/bulletinMain/BulletinMainHeader.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { Image, TouchableOpacity, StyleSheet, View, Text, SafeAreaView, Platform, StatusBar } from 'react-native';
+
+function BulletinMainHeader() {
+    return (
+        <View style={styles.outerContainer}>
+            <View style={styles.headerContainer}>
+                <TouchableOpacity onPress={() => console.log('Back Button Pressed!')}>
+                    <Image 
+                        source={require('../../assets/backArrowKey.png')}
+                        style = {styles.iconStyleBack}
+                    />
+                </TouchableOpacity>
+                <Text style={styles.headerText}>Utama</Text>
+                <TouchableOpacity onPress={() => console.log('Settings Button Pressed!')}>
+                    <Image 
+                        source={require('../../assets/settingsIcon.png')}
+                        style = {styles.iconStyleSetting}
+                    />
+                </TouchableOpacity>
+            </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    outerContainer: {
+        backgroundColor: '#9448DA',
+        borderBottomLeftRadius: 15,
+        borderBottomRightRadius: 15,
+        paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight-20: 0,
+    },
+    headerContainer: {
+        height: 68, 
+        flexDirection: 'row',
+        alignItems: 'center', 
+        justifyContent: 'space-evenly',
+        paddingHorizontal: 15, 
+    },
+    headerText: {
+        color: '#F5F5F5',
+        fontWeight: '700',
+        fontSize: 20,
+        width: '20%',
+        textAlign: 'center',
+        marginRight: '50%' 
+
+    },
+    iconStyleBack: {
+        width: 25, 
+        height: 25, 
+        resizeMode: 'contain',
+    },
+    iconStyleSetting: {
+        width: 25, 
+        height: 25, 
+        resizeMode: 'contain',
+        backgroundColor: 'transparent',
+    },
+});
+
+export default BulletinMainHeader;
--- app/screens/bulletinMain/index.js
+++ app/screens/bulletinMain/index.js
@@ -1,21 +1,23 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, SafeAreaView } from 'react-native';
+import Header from './BulletinMainHeader';
 
-function ChatScreen() {
+function BulletinMain() {
   return (
-    <View style={styles.container}>
-      <Text>This is the Chat Screen</Text>
-      {/* You can add any placeholder content here */}
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.headerContainer}>
+        <Header />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
+    flex: 1, 
+    backgroundColor: '#9448DA',
+    backgroundColor: 'transparent',
+},
 });
 
-export default ChatScreen;
+export default BulletinMain;

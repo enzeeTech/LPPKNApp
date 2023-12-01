ea745bc08c188ec2b8196f2891a882d4f03809a8
Dilshan Jayasinghe <dilshan.sj@socio.com.my>
2023-12-01 17:56:01 +0800

    Sub header component added
    TO-DO
    - Create saring component
    - Add navigation to saring

--- app/screens/bulletinMain/index.js
+++ app/screens/bulletinMain/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, StyleSheet, SafeAreaView } from 'react-native';
+import { View, Text, StyleSheet, SafeAreaView, Image, TouchableOpacity } from 'react-native';
 import Header from './BulletinMainHeader';
 
 function BulletinMain() {
@@ -7,6 +7,27 @@ function BulletinMain() {
     <SafeAreaView style={styles.container}>
       <View style={styles.headerContainer}>
         <Header />
+      </View>
+      <View style={styles.subHeaderContainer}>
+        <TouchableOpacity style={styles.searchTab} onPress={() => console.log('Search Tab Pressed!')}>
+          <View style={styles.searchIconContainer}>
+            <Image 
+              source={require('../../assets/searchButton.png')}
+              style = {styles.searchIcon}
+            />
+            <View style={styles.seachTextContainer}>
+              <Text style={styles.searchText}>
+                Masukkan carian
+              </Text>
+            </View>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.sarineIconContainer} onPress={() => console.log('Sarine Button Pressed!')}> 
+          <Image
+              source={require('../../assets/sarineButton.png')}
+              style = {styles.sarineIcon}
+            />
+        </TouchableOpacity>
       </View>
     </SafeAreaView>
   );
@@ -17,7 +38,59 @@ const styles = StyleSheet.create({
     flex: 1, 
     backgroundColor: '#9448DA',
     backgroundColor: 'transparent',
-},
+  },
+  subHeaderContainer: {
+    flexDirection: 'row',
+    backgroundColor: '#FFFFFF',
+    height: '8%',
+    width: '100%',
+  },
+  searchTab: {
+    width: '75%',
+    height: '65%',
+    marginTop: '3%',
+    marginLeft: '5%',
+    borderBottomLeftRadius: 10,
+    borderBottomRightRadius: 10,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    borderWidth: 2,
+    borderColor: '#CBCBCB',
+  },
+  searchIconContainer: {
+    width: '15%',
+    height: '100%',
+    flexDirection: 'row',
+  },
+  searchIcon: {
+    width: '60%',
+    height: '60%',
+    resizeMode: 'contain',
+    marginLeft: '25%',
+    marginTop: '15%',
+  },
+  sarineIcon: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'contain',
+  },
+  seachTextContainer: {
+    width: 200,
+    height: '100%',
+  },
+  searchText: {
+    color: '#A6A6A6',
+    fontSize: 14,
+    marginTop: '5%',
+    marginLeft: '8%',
+  },
+  sarineIconContainer: {
+    width: '13%',
+    height: '65%',
+    marginTop: '3%',
+    marginLeft: '2%',
+  },
+
 });
 
 export default BulletinMain;

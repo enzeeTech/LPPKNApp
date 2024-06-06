37fd8cd1473bd54cb6a152480470a6b037165f08
Dilshan Jayasinghe <dilshan.sj@socio.com.my>
2024-06-06 22:43:30 +0800

    Updated app Icon

--- app/screens/servicesScreen/screens/KeluargaKerja.js
+++ app/screens/servicesScreen/screens/KeluargaKerja.js
@@ -148,10 +148,11 @@ const KeluargaKerja = ({ navigation }) => {
                             <Text style={styles.buttonTextOne}>Hubungi Pejabat LPPKN Negeri</Text>
                         </TouchableOpacity>
                     </View>
+                    <View style={{height: 110, backgroundColor: '#FFF'}}></View>
+                    {/* View created to add padding */}
+                    <View style={{height: 50, backgroundColor: '#FFF'}}></View>
                 </View>
-                <View style={{height: 110, backgroundColor: '#FFF'}}></View>
-                 {/* View created to add padding */}
-                 <View style={{height: 50, backgroundColor: '#FFF'}}></View>
+                
 
             </ScrollView>
         </SafeAreaView>
